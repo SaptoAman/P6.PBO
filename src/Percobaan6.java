c04bc8f6466ec501ab02406b06a954267604882c
AdiRizkyR <adirizkyramadhan912@gmail.com>
2022-04-27 03:33:23 +0700

    Method local inner class

--- src/Percobaan6.java
+++ src/Percobaan6.java
@@ -1,21 +1,15 @@
-//Membuat outer class
 class MyOuterClassDemo {
-    //Membuat variabel
     private final int x = 1;
 
-    //instansi method dari outer class
     public void doThings () {
         final String name = "Variabel Local";
 
-        //inner class didefinisikan di dalam method dari outer class
         class MyInnerClassDemo {
-            //membuat method
             public void seeOuter() {
                 System.out.println("Nilai Outer Dari X = " +x);
                 System.out.println("Nilai Dari Nama = " +name);
             }
         }
-        //Mengakses inner class
         MyInnerClassDemo inner = new MyInnerClassDemo();
         inner.seeOuter();
     }
@@ -23,7 +17,6 @@ class MyOuterClassDemo {
 
 public class Percobaan6 {
     public static void main(String[] args) {
-        //Membuat objek
         MyOuterClassDemo myOuter = new MyOuterClassDemo();
         myOuter.doThings();
     }
