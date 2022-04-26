e919043a0d1f15d50b17ced5f225c61e3b084226
AdiRizkyR <adirizkyramadhan912@gmail.com>
2022-04-27 03:23:11 +0700

    implemenatasi multiple interface

---
+++ src/Percobaan4/FirstInterface.java
@@ -0,0 +1,10 @@
+package Percobaan4;
+
+public interface FirstInterface {
+    //Membuat variabel
+    int NUMBER = 13;
+
+    //Membuat method
+    void printHello();
+    int doubleNumberA();
+}
---
+++ src/Percobaan4/SecondInterface.java
@@ -0,0 +1,10 @@
+package Percobaan4;
+
+public interface SecondInterface {
+    //Membuat variabel
+    int NUMBER = 11;
+
+    //Membuat method
+    void printHello();
+    int doubleNumberB();
+}
